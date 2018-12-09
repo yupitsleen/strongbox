688d86c54d7b614c13b46d5447e2e09a107babbd
carlspring <carlspring@gmail.com>
2018-12-09 15:30:35 +0000

    SB-1323: Make the strongbox-web-core's tests work in parallel
    - Fixed the `CustomAntPathMatcherTest`.

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/CustomAntPathMatcherTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/CustomAntPathMatcherTest.java
@@ -10,11 +10,13 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 
 /**
  * Several test cases just to make sure that path variable parsing is correct.
@@ -23,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  */
 @IntegrationTest
 @ExtendWith(SpringExtension.class)
+@Execution(CONCURRENT)
 public class CustomAntPathMatcherTest
 {
 
@@ -32,52 +35,44 @@ public class CustomAntPathMatcherTest
     @Named("customAntPathMatcher")
     CustomAntPathMatcher antPathMatcher;
 
+
     @Test
     public void testSimplePathMatching()
-            throws Exception
     {
         final String artifactPath = "org/carlspring/fake/mock/jar/par/test-jar-1.0.5.123.3232.2221123.3.2.1.jar";
-        final String pattern =
-                "/storages/{storageId}/{repositoryId}/{path:" + CustomAntPathMatcher.TWO_STARS_ANALOGUE + "}";
         final String path = "/storages/storageId/repositoryId/" + artifactPath;
 
-        doTest(artifactPath, path, pattern);
+        doTest(artifactPath, path);
     }
 
     @Test
     public void testPathMatchingForMultipleSubPaths()
     {
         final String artifactPath = ".trash/org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar";
-        final String pattern =
-                "/storages/{storageId}/{repositoryId}/{path:" + CustomAntPathMatcher.TWO_STARS_ANALOGUE + "}";
         final String path = "/storages/storage0/releases-with-trash/" + artifactPath;
 
-        doTest(artifactPath, path, pattern);
+        doTest(artifactPath, path);
     }
 
     @Test
     public void testPathMatchingForComplexPath()
     {
         final String artifactPath = "org/test/mock/spring/23-123f--,,342&*#$/~276409~!$#%^&*(-=/3/2/1/3.jar";
-        final String pattern =
-                "/storages/{storageId}/{repositoryId}/{path:" + CustomAntPathMatcher.TWO_STARS_ANALOGUE + "}";
         final String path = "/storages/storage0/repositoryId/" + artifactPath;
 
-        doTest(artifactPath, path, pattern);
+        doTest(artifactPath, path);
     }
 
-    private void doTest(String artifactPath,
-                        String path,
-                        String pattern)
+    private void doTest(String artifactPath, String path)
     {
-
         final Map<String, String> uriTemplateVariables = new HashMap<>();
 
-        antPathMatcher.doMatch(pattern, path, true, uriTemplateVariables);
+        antPathMatcher.doMatch("/storages/{storageId}/{repositoryId}/{path:.+}", path, true, uriTemplateVariables);
 
         String pathVariable = uriTemplateVariables.get("path");
-        assertNotNull(pathVariable, "Unable to find path variable. uriTemplateVariables " + uriTemplateVariables);
 
+        assertNotNull(pathVariable, "Unable to find path variable. uriTemplateVariables " + uriTemplateVariables);
         assertEquals(artifactPath, pathVariable);
     }
+
 }
