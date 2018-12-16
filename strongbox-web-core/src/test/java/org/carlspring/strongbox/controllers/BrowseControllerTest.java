5a733c4d6c02c574b4e0e205b37d88d5d5bb210a
carlspring <carlspring@gmail.com>
2018-12-16 02:20:45 +0000

    SB-1323: Make the strongbox-web-core's tests work in parallel
    - Fixed the `BrowseControllerTest`.

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/BrowseControllerTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/BrowseControllerTest.java
@@ -3,7 +3,6 @@ package org.carlspring.strongbox.controllers;
 import org.carlspring.strongbox.config.IntegrationTest;
 import org.carlspring.strongbox.domain.DirectoryListing;
 import org.carlspring.strongbox.providers.layout.Maven2LayoutProvider;
-import org.carlspring.strongbox.resource.ConfigurationResourceResolver;
 import org.carlspring.strongbox.rest.common.MavenRestAssuredBaseTest;
 import org.carlspring.strongbox.storage.repository.MavenRepositoryFactory;
 import org.carlspring.strongbox.storage.repository.MutableRepository;
@@ -11,21 +10,14 @@ import org.carlspring.strongbox.storage.repository.RepositoryPolicyEnum;
 import org.carlspring.strongbox.xml.configuration.repository.MutableMavenRepositoryConfiguration;
 
 import javax.inject.Inject;
-import javax.xml.bind.JAXBException;
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.UndeclaredThrowableException;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
@@ -34,19 +26,17 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * @author Guido Grazioli
  */
-
 @IntegrationTest
 @ExtendWith(SpringExtension.class)
 public class BrowseControllerTest
         extends MavenRestAssuredBaseTest
 {
-    
-    private static final Logger logger = LoggerFactory.getLogger(BrowseControllerTest.class);
-    
+
     private static final String REPOSITORY = "browsing-test-repository";
     
     @Inject
     private MavenRepositoryFactory mavenRepositoryFactory;
+
 
     @BeforeAll
     public static void setup()
@@ -59,6 +49,7 @@ public class BrowseControllerTest
     {
         Set<MutableRepository> repositories = new LinkedHashSet<>();
         repositories.add(createRepositoryMock(STORAGE0, REPOSITORY, Maven2LayoutProvider.ALIAS));
+
         return repositories;        
     }
     
@@ -77,38 +68,8 @@ public class BrowseControllerTest
         repository.setRepositoryConfiguration(mavenRepositoryConfiguration);
 
         createRepository(STORAGE0, repository);
-        
-        generateArtifact(getRepositoryBasedir(STORAGE0, REPOSITORY).getAbsolutePath(),
-                         "org.carlspring.strongbox.browsing:test-browsing",
-                         new String[]{ "1.1",
-                                       "3.2"  
-                         }
-        );      
-    } 
-    
-    @Override
-    @AfterEach
-    public void shutdown()
-    {
-        try
-        {
-            closeIndexersForRepository(STORAGE0, REPOSITORY);
-            removeRepositories();
-            cleanUp(getRepositoriesToClean());
-        }
-        catch (Exception e)
-        {
-            throw new UndeclaredThrowableException(e);
-        }
-        super.shutdown();
     }
 
-    private void removeRepositories()
-            throws IOException, JAXBException
-    {
-        removeRepositories(getRepositoriesToClean());
-    }
-    
     @Test
     public void testGetStorages()
             throws Exception
@@ -173,9 +134,7 @@ public class BrowseControllerTest
                                      .asString();
 
         assertTrue(htmlResponse.contains(REPOSITORY), "Returned HTML is incorrect");
-}
-                                 
-
+    }
 
     @Test
     public void testGetRepositoriesWithStorageNotFound()
@@ -200,6 +159,12 @@ public class BrowseControllerTest
     public void testRepositoryContents()
             throws Exception
     {
+        generateArtifact(getRepositoryBasedir(STORAGE0, REPOSITORY).getAbsolutePath(),
+                         "org.carlspring.strongbox.browsing:test-browsing",
+                         new String[]{ "1.1",
+                                       "3.2"
+                         });
+
         String url = getContextBaseUrl() + BrowseController.ROOT_CONTEXT + "/" + STORAGE0 + "/" + REPOSITORY
                      + "/org/carlspring/strongbox/browsing/test-browsing/1.1";
         
@@ -220,8 +185,8 @@ public class BrowseControllerTest
                                      .prettyPeek()
                                      .asString();
 
-        String link = getContextBaseUrl() + "/storages/"
-                      + STORAGE0 + "/" + REPOSITORY + "/org/carlspring/strongbox/browsing/test-browsing/1.1/test-browsing-1.1.jar";
+        String link = getContextBaseUrl() + "/storages/" + STORAGE0 + "/" + REPOSITORY +
+                      "/org/carlspring/strongbox/browsing/test-browsing/1.1/test-browsing-1.1.jar";
 
         assertTrue(htmlResponse.contains(link), "Expected to have found [ " + link + " ] in the response html");
     }
@@ -263,4 +228,5 @@ public class BrowseControllerTest
                .then()
                .statusCode(404);        
     }
+
 }
