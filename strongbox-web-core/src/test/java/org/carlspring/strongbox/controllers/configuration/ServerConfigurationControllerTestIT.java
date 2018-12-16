e5f71587d807b39eb7c790c439788a1b45c6ce36
carlspring <carlspring@gmail.com>
2018-12-16 01:28:17 +0000

    SB-1323: Make the strongbox-web-core's tests work in parallel
    - Fixed the `ServerConfigurationControllerTestIT`.

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/configuration/ServerConfigurationControllerTestIT.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/configuration/ServerConfigurationControllerTestIT.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -26,12 +27,14 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 
 /**
  * @author Pablo Tirado
  */
 @IntegrationTest
 @ExtendWith(SpringExtension.class)
+@Execution(CONCURRENT)
 public class ServerConfigurationControllerTestIT
         extends RestAssuredBaseTest
 {
