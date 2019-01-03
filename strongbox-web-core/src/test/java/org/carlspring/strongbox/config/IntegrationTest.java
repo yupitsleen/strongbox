79ca62af4cd1a805d987f1b05308c25170fd0d31
Przemyslaw Fusik <przemyslaw.fusik@gmail.com>
2019-01-03 23:41:15 +0100

    SB-1323 concurrent scenario and RestAssuredMockMvc

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/config/IntegrationTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/config/IntegrationTest.java
@@ -7,24 +7,29 @@ import org.carlspring.strongbox.configuration.MutableConfiguration;
 import org.carlspring.strongbox.cron.services.CronJobSchedulerService;
 import org.carlspring.strongbox.cron.services.CronTaskConfigurationService;
 import org.carlspring.strongbox.data.CacheManagerTestExecutionListener;
-import org.carlspring.strongbox.rest.common.RestAssuredTestExecutionListener;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
 import static org.mockito.ArgumentMatchers.any;
 
 
@@ -39,12 +44,11 @@ import static org.mockito.ArgumentMatchers.any;
 @SpringBootTest(classes = { StrongboxSpringBootApplication.class,
                             MockedRemoteRepositoriesHeartbeatConfig.class,
                             IntegrationTest.TestConfig.class,
-                            RestAssuredConfig.class})
+                            RestAssuredConfig.class })
 @WebAppConfiguration("classpath:")
 @WithUserDetails(value = "admin")
 @ActiveProfiles(profiles = "test")
-@TestExecutionListeners(listeners = { RestAssuredTestExecutionListener.class,
-                                      CacheManagerTestExecutionListener.class }, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+@TestExecutionListeners(listeners = { CacheManagerTestExecutionListener.class }, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
 @Execution(ExecutionMode.SAME_THREAD)
 public @interface IntegrationTest
 {
@@ -78,6 +82,27 @@ public @interface IntegrationTest
                    .store(any(MutableConfiguration.class));
 
             return configurationFileManager;
+        }
+
+        @EventListener
+        void handleContextRefreshedEvent(ContextRefreshedEvent event)
+        {
+            ApplicationContext applicationContext = event.getApplicationContext();
+            if (applicationContext instanceof WebApplicationContext)
+            {
+                RestAssuredMockMvc.webAppContextSetup((WebApplicationContext) applicationContext);
+            }
+
+        }
+
+        @EventListener
+        void handleContextStoppedEvent(ContextStoppedEvent event)
+        {
+            ApplicationContext applicationContext = event.getApplicationContext();
+            if (applicationContext instanceof WebApplicationContext)
+            {
+                RestAssuredMockMvc.reset();
+            }
         }
     }
 
